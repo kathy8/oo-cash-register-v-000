81723b3a87a6913b29748c3ef0b26d7248e8d0fd
Katherine Gruenberg <katherine.gruenberg@gmail.com>
2017-10-18 19:25:34 +0000

    automatically backed up by learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,10 @@
+class cash_register_spec
+  attr_accessor :total, :discount, :title, :price, :items, :last_transaction
 
+  def initialize(discount) = 0
+    @total = 0
+    @dsicount= discount
+
+    @items = []
+  end
+  
